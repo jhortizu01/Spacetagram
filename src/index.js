e3935153267fd32200183df72a683a8bb4a1d36d
Janika Hortizuela <jhortizuela21@gmail.com>
2021-09-21 10:43:36 -0600

    Final refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles/index.scss";
-import SpaceRepository from "./classes/spaceRepository.js";
+
 import SpaceObject from "./classes/spaceObject.js";
 import "./images/like.svg"
 import "./images/unlike.svg"
@@ -7,12 +7,13 @@ import "./images/space-background.jpeg"
 import "./images/astronaut.gif"
 import dayjs from 'dayjs'
 
-let spaceRepository, spaceObject
-
+let spaceObject
 let cardContainer = document.getElementById('cardContainer')
 let submit = document.getElementById('submitButton')
 
-function fetchAPI() {
+//Functions//
+
+const fetchAPI = () => {
   let API_KEY = "SjnTawcUeG6oSoWsk4yVaMlKBouZUGWGX1yoNWbj"
   return fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=20`)
    .then(response => response.json())
@@ -20,8 +21,7 @@ function fetchAPI() {
    .catch(error => console.log(error))
  }
 
- function instantiateData(data) {
-   spaceRepository = new SpaceRepository(data)
+const instantiateData = (data) => {
    spaceObject = data.map(object => {
     return new SpaceObject(object)
    })
@@ -29,7 +29,7 @@ function fetchAPI() {
    showCards(spaceObject)
  }
 
- function showCards(data) {
+const showCards = (data) => {
   data.map(object =>
     {
     const spaceCard = document.createElement('div');
@@ -51,22 +51,15 @@ function fetchAPI() {
 
     cardContainer.innerHTML += cardContent
   })
- }
- 
+} 
 
- window.addEventListener('load', () => {
-  fetchAPI()
- })
-
- submit.addEventListener('click', getNewCards)
-
- function getNewCards() {
+const getNewCards = () => {
    let startDateValue = document.getElementById('startDate').value
    let endDateValue = document.getElementById('endDate').value
    validateData(startDateValue, endDateValue)
  }
 
- function validateData(start, end) {
+const validateData = (start, end) => {
   let today = new Date()
   let wrongStartError = document.getElementById('wrongStart')
   let wrongEndError = document.getElementById('wrongEnd')
@@ -83,7 +76,7 @@ function fetchAPI() {
    }
  }
 
- function fetchNewDates(start, end) {
+const fetchNewDates = (start, end) => {
   let API_KEY = "SjnTawcUeG6oSoWsk4yVaMlKBouZUGWGX1yoNWbj"
   return fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${start}&end_date=${end}`)
    .then(response => response.json())
@@ -91,19 +84,34 @@ function fetchAPI() {
    .catch(error => console.log(error))
  }
 
- function clearCards() {
-   while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
+const clearCards = () => {
+  while (container.firstChild) {
+   container.removeChild(container.firstChild);
  }
+}
 
+async function copyPageUrl() {
+  var copyText = document.getElementById('imageLink').value;
+  console.log(copyText)
+  try {
+    await navigator.clipboard.writeText(copyText);
+    console.log('Page URL copied to clipboard');
+  } catch (err) {
+    console.error('Failed to copy: ', err);
+  }
+}
 
+//Event Listeners
+
+window.addEventListener('load', () => {
+  fetchAPI()
+ })
+
+submit.addEventListener('click', getNewCards)
 
 cardContainer.addEventListener('click', (e) => {
   let eventButton = e.target
   let likeBtn = document.getElementById('likeButton')
- // let ariaLabel = eventButton.ariaLabel;
-
 
   if(eventButton.classList.contains('unlike')) {
     eventButton.classList.remove('unlike')
@@ -117,18 +125,3 @@ cardContainer.addEventListener('click', (e) => {
       copyPageUrl()
   }
 })
-
-
-
-async function copyPageUrl() {
-  var copyText = document.getElementById('imageLink').value;
-  console.log(copyText)
-  try {
-    await navigator.clipboard.writeText(copyText);
-    console.log('Page URL copied to clipboard');
-  } catch (err) {
-    console.error('Failed to copy: ', err);
-  }
-}
-
-//Jun 16, 1995 and Sep 19, 2021
